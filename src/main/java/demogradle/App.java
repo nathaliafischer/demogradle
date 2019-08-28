a0edaab687e2e94a750a4bf4086ff5ac77580d28
Nathalia <nathyh.fischer@gmail.com>
2019-08-28 17:08:38 -0300

    Commit inicial da aplicação.

---
+++ src/main/java/demogradle/App.java
@@ -0,0 +1,10 @@
+package demogradle;
+public class App {
+    public String getGretting(){
+        return "Hello World.";
+    }
+
+    public static void main (String[] args){
+        System.out.println(new App().getGretting());
+    }
+}
